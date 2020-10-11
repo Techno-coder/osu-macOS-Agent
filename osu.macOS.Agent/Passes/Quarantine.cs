a16a8d1ac80b3637303ed021648e265534dbd61d
Technocoder <8334328+Techno-coder@users.noreply.github.com>
2020-10-12 04:40:43 +1300

    feat: Add unexpected attribute check on quarantine pass

--- osu.macOS.Agent/Passes/Quarantine.cs
+++ osu.macOS.Agent/Passes/Quarantine.cs
@@ -8,14 +8,15 @@ namespace osu.macOS.Agent.Passes
 		public Entry Apply(Instance instance)
 		{
 			var data = Instance.ExecuteCommand($"{Name} '{instance.rootPath}'");
+			if (data.Length == 0) return new Entry("Quarantine attribute is absent");
 			return data.Contains(Attribute)
 				? new Entry("Quarantine attribute is present", Severity.Error)
-				: new Entry("Quarantine attribute is absent");
+				: new Entry($"Unexpected attribute data:\n{data}", Severity.Warning);
 		}
 
 		public Status Repair(Instance instance)
 		{
-			Instance.ExecuteCommand($"{Name} -dr {Attribute} '{instance.rootPath}'");
+			Instance.ExecuteCommand($"{Name} -c '{instance.rootPath}'");
 			return Apply(instance).severity != Severity.None ? Status.Failed : Status.Fixed;
 		}
 	}
