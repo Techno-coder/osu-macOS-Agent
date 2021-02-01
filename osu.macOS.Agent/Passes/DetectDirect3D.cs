7a86ff8256b96b46a60802fec7adecb5553441df
Technocoder <8334328+Techno-coder@users.noreply.github.com>
2021-02-01 16:05:58 +1300

    feat: unconditionally disable Direct3D check as unstable
    Thanks @Gcenx

--- osu.macOS.Agent/Passes/DetectDirect3D.cs
+++ osu.macOS.Agent/Passes/DetectDirect3D.cs
@@ -4,18 +4,12 @@ namespace osu.macOS.Agent.Passes
 {
 	public class DetectDirect3D : IPass
 	{
-		private static readonly NSOperatingSystemVersion HighSierra = new NSOperatingSystemVersion(10, 13, 0);
 		private const string TargetKey = "Try To Use GPU Info";
 
-		public Entry Apply(Instance instance)
-		{
-			var enabled = (bool) (NSNumber) instance.configuration[TargetKey];
-			if (!enabled) return new Entry("Detect Direct3D is disabled");
-
-			var sierra = NSProcessInfo.ProcessInfo.IsOperatingSystemAtLeastVersion(HighSierra);
-			var severity = sierra && !Instance.RequiresLongArchitecture() ? Severity.Error : Severity.None;
-			return new Entry("Detect Direct3D is enabled", severity);
-		}
+		public Entry Apply(Instance instance) =>
+			(bool) (NSNumber) instance.configuration[TargetKey]
+				? new Entry("Detect Direct3D is enabled", Severity.Error)
+				: new Entry("Detect Direct3D is disabled");
 
 		public Status Repair(Instance instance)
 		{
